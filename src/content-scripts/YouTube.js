69b00999e68c03a33ff23b95e2499391d5231001
MohamedNoordin <moh.noordin10@gmail.com>
2023-09-27 23:35:47 +0300

    Fix asynchronous calls timing issues

--- src/content-scripts/YouTube.js
+++ src/content-scripts/YouTube.js
@@ -10,7 +10,7 @@ class YouTubeObserver {
 		// Create a new MutationObserver instance
 		// To be notified when a new comment has been added to the DOM
 		const observer = new MutationObserver((mutations, observer) => {
-			mutations.forEach(async(mutation) => {
+			mutations.forEach((mutation) => {
 				const addedNodes = Array.from(mutation.addedNodes);
 				const comments = addedNodes.filter(node => node.tagName === "YTD-COMMENT-RENDERER");
 				const featuredComments = Array.from(document.getElementsByTagName("YTD-AUTHOR-COMMENT-BADGE-RENDERER"));
@@ -27,43 +27,45 @@ class YouTubeObserver {
 		
 	observeForfeaturedComments(featuredComments) {
 		if (featuredComments.length > 0) {
-			featuredComments.forEach(async(comment) => {
+			featuredComments.forEach((comment) => {
 				const featuredCommentChannelAnchor = comment.querySelector("A");
 				if (featuredCommentChannelAnchor) {
 					const featuredCommentChannelHref = featuredCommentChannelAnchor.getAttribute("href");
 					const featuredCommentChannelIDFormattedString = comment.querySelector("YT-FORMATTED-STRING");
 					if (featuredCommentChannelIDFormattedString && !this.featuredHandledComments.includes(featuredCommentChannelHref)) {
 						this.featuredHandledComments.push(featuredCommentChannelHref);
-						const response = await fetch("https://www.youtube.com/" + featuredCommentChannelHref);
-						const html = await response.text();
-						// Initialize the DOM parser
-						let parser = new DOMParser();
+						const response = fetch("https://www.youtube.com/" + featuredCommentChannelHref)
+						.then(res => res.text())
+						.then((data) => {
+							// Initialize the DOM parser
+							let parser = new DOMParser();
 
-						// Parse the text
-						var doc = parser.parseFromString(html, "text/html");
+							// Parse the text
+							var doc = parser.parseFromString(data, "text/html");
 
-						// Get the channel name (Channel name - YouTube)
-						const channelName = doc.title.split(" - ")[0];
+							// Get the channel name (Channel name - YouTube)
+							const channelName = doc.title.split(" - ")[0];
 
-						// Check if the comment is from the channel owner
-						let isOwner = false;
-						const channelOwnerCommentRendererH3 = comment.parentElement.parentElement.getElementsByTagName("H3")[0];
-						const channelOwnerCommentChannelAnchor = channelOwnerCommentRendererH3.querySelector("A");
-						if (channelOwnerCommentChannelAnchor) {
-							const channelOwnerCommentSpan = channelOwnerCommentChannelAnchor.querySelector("span");
-							if (channelOwnerCommentSpan && channelOwnerCommentSpan.classList.contains("channel-owner")) {
-								// Set the channel name next to the ID with the owner badge
-								const commentChannelID = 
-									featuredCommentChannelIDFormattedString.textContent = channelName + " ( 👑 " + featuredCommentChannelIDFormattedString.textContent + " )";
-								isOwner = true;
+							// Check if the comment is from the channel owner
+							let isOwner = false;
+							const channelOwnerCommentRendererH3 = comment.parentElement.parentElement.getElementsByTagName("H3")[0];
+							const channelOwnerCommentChannelAnchor = channelOwnerCommentRendererH3.querySelector("A");
+							if (channelOwnerCommentChannelAnchor) {
+								const channelOwnerCommentSpan = channelOwnerCommentChannelAnchor.querySelector("span");
+								if (channelOwnerCommentSpan && channelOwnerCommentSpan.classList.contains("channel-owner")) {
+									// Set the channel name next to the ID with the owner badge
+									const commentChannelID = 
+										featuredCommentChannelIDFormattedString.textContent = channelName + " ( 👑 " + featuredCommentChannelIDFormattedString.textContent + " )";
+									isOwner = true;
+								}
 							}
-						}
 
-						if (!isOwner) {
-							// Set the channel name next to the ID
-							const commentChannelID = 
-								featuredCommentChannelIDFormattedString.textContent = channelName + " ( 🙌 " + featuredCommentChannelIDFormattedString.textContent + " )";
-						}
+							if (!isOwner) {
+								// Set the channel name next to the ID
+								const commentChannelID = 
+									featuredCommentChannelIDFormattedString.textContent = channelName + " ( 🙌 " + featuredCommentChannelIDFormattedString.textContent + " )";
+							}
+						});
 					}
 				}
 			});
@@ -72,27 +74,29 @@ class YouTubeObserver {
 
 	observeForComments(comments) {
 		if (comments.length > 0) {
-			comments.forEach(async(comment) => {
+			comments.forEach((comment) => {
 				const commentRendererH3 = comment.querySelector("H3");
 				const commentChannelAnchor = commentRendererH3.querySelector("A");
 				if (commentChannelAnchor) {
 					const commentChannelHref = commentChannelAnchor.getAttribute("href");
 					const commentChannelIDSpan = commentChannelAnchor.querySelector("span");
 					if (commentChannelIDSpan) {
-						const response = await fetch("https://www.youtube.com/" + commentChannelHref);
-						const html = await response.text();
-						// Initialize the DOM parser
-						let parser = new DOMParser();
+						const response = fetch("https://www.youtube.com/" + commentChannelHref)
+						.then(res => res.text())
+						.then((data) => {
+							// Initialize the DOM parser
+							let parser = new DOMParser();
 
-						// Parse the text
-						var doc = parser.parseFromString(html, "text/html");
+							// Parse the text
+							var doc = parser.parseFromString(data, "text/html");
 
-						// Get the channel name (Channel name - YouTube)
-						const channelName = doc.title.split(" - ")[0];
+							// Get the channel name (Channel name - YouTube)
+							const channelName = doc.title.split(" - ")[0];
 
-						// Set the channel name next to the ID
-						const commentChannelID = commentChannelIDSpan.textContent;
-						commentChannelIDSpan.textContent = channelName + " (" + commentChannelID + ")";
+							// Set the channel name next to the ID
+							const commentChannelID = commentChannelIDSpan.textContent;
+							commentChannelIDSpan.textContent = channelName + " (" + commentChannelID + ")";
+						});
 					}
 				}
 			});
